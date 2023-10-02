a76806d27353a80b06d06e32283ad7eb7cc7716f
Lucas Gabriel Macêdo <lucasgabriel060405@gmail.com>
2023-10-01 21:10:41 -0300

    [Add/Create] Spotify HomePage - MacDesktop - Frontend

---
+++ src/app/page.tsx
@@ -0,0 +1,112 @@
+import { ChevronLeft, ChevronRight, PlayIcon, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize2} from 'lucide-react'
+import Image from 'next/image'
+import { Sidebar } from './components/Sidebar'
+import { Footer } from './components/Footer'
+
+export default function Home() {
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <Sidebar />
+
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-4">
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronLeft />
+            </button>
+            <button className='rounded-full bg-black/240 p-1'>
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
+
+        <div className='grid grid-cols-3 gap-4 mt-4'>
+          <a href='#' className='bg-white/8 group rounded items-center flex gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+            <Image src="/kayblack.jpg" width={104} height={104} alt="Capa do Album KayBlack" />
+            <strong>Contradições</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <Play />
+            </button>
+          </a>
+
+          <a href='#' className='bg-white/8 group rounded items-center flex gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+            <Image src="/kayblack.jpg" width={104} height={104} alt="Capa do Album KayBlack" />
+            <strong>Contradições</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <Play />
+            </button>
+          </a>
+
+          <a href='#' className='bg-white/8 group rounded items-center flex gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+            <Image src="/kayblack.jpg" width={104} height={104} alt="Capa do Album KayBlack" />
+            <strong>Contradições</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <Play />
+            </button>
+          </a>
+
+          <a href='#' className='bg-white/8 group rounded items-center flex gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+            <Image src="/kayblack.jpg" width={104} height={104} alt="Capa do Album KayBlack" />
+            <strong>Contradições</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <Play />
+            </button>
+          </a>
+
+          <a href='#' className='bg-white/8 group rounded items-center flex gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+            <Image src="/kayblack.jpg" width={104} height={104} alt="Capa do Album KayBlack" />
+            <strong>Contradições</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <Play />
+            </button>
+          </a>
+
+          <a href='#' className='bg-white/8 group rounded items-center flex gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+            <Image src="/kayblack.jpg" width={104} height={104} alt="Capa do Album KayBlack" />
+            <strong>Contradições</strong>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+              <Play />
+            </button>
+          </a>
+        </div>
+
+        <h2 className='font-semibold text-2xl mt-10'>Made For Lucas Gabriel Macêdo</h2>
+        
+        <div className='grid grid-cols-8 gap-4 mt-4'>
+          <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src="/kayblack.jpg" className='w-full' width={104} height={104} alt="Capa do Album KayBlack" />
+          <strong className='font-semibold'> Daily Mix</strong>
+          <span className='text-sm text-zinc-400'>Kayblack, MajorRD, Felipe Ret and more</span>
+          </a>
+
+          <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src="/kayblack.jpg" className='w-full' width={104} height={104} alt="Capa do Album KayBlack" />
+          <strong className='font-semibold'> Daily Mix</strong>
+          <span className='text-sm text-zinc-400'>Kayblack, MajorRD, Felipe Ret and more</span>
+          </a>
+
+          <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src="/kayblack.jpg" className='w-full' width={104} height={104} alt="Capa do Album KayBlack" />
+          <strong className='font-semibold'> Daily Mix</strong>
+          <span className='text-sm text-zinc-400'>Kayblack, MajorRD, Felipe Ret and more</span>
+          </a>
+
+          <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src="/kayblack.jpg" className='w-full' width={104} height={104} alt="Capa do Album KayBlack" />
+          <strong className='font-semibold'> Daily Mix</strong>
+          <span className='text-sm text-zinc-400'>Kayblack, MajorRD, Felipe Ret and more</span>
+          </a>
+
+          <a href='#' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+          <Image src="/kayblack.jpg" className='w-full' width={104} height={104} alt="Capa do Album KayBlack" />
+          <strong className='font-semibold'> Daily Mix</strong>
+          <span className='text-sm text-zinc-400'>Kayblack, MajorRD, Felipe Ret and more</span>
+          </a>
+        </div>
+        </main>
+      </div>
+        <Footer />
+    </div>
+  )
+}
